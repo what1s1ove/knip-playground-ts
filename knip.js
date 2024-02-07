9a85fb28ab727d8f6c96fa8308ddd2cb7f0326e3
what1s1ove <zubko.2007@gmail.com>
2024-02-07 17:53:49 +0200

    chore: edit types key for shared module

--- knip.js
+++ knip.js
@@ -4,12 +4,9 @@ const config = {
 		'apps/app': {
 			entry: ['index.ts'],
 		},
-		// 'packages/shared': {
-		// 	entry: ['src/index.ts'],
-		// },
 		'packages/shared': {
-			entry: ['build/index.d.ts'],
-			ignore: ['src/**/*', 'build/helpers/**/*.d.ts'],
+			entry: ['src/index.ts'],
+			includeEntryExports: true,
 		},
 	},
 }
